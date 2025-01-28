3ed5a34941428261fa549d26642026f4125a7202
Jim_Liu <jim.t90615@gmail.com>
2025-01-28 11:26:50 +0800

    stm8: add clock source
    add stm8 16MHZ clock source
    
    Signed-off-by: Jim_Liu <jim.t90615@gmail.com>

--- main.c
+++ main.c
@@ -4,7 +4,26 @@
  */
 
 
+#include "stm8s.h"
+#include "stm8s_conf.h"
+
+void Clock_Config(void)
+{
+	//enable internal HSI clock(16MHZ)
+	CLK_HSICmd(ENABLE);
+
+	//make sure internal clock(HSI) is stable
+	while (CLK_GetFlagStatus(CLK_FLAG_HSIRDY) == RESET);
+
+	//set HSI DIV(High speed internal clock prescaler: 1)
+	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+
+	//enable i2c function
+	//CLK_PeripheralClockConfig(CLK_PERIPHERAL_I2C, ENABLE);
+}
+
 main()
 {
+	Clock_Config();
 	while (1);
 }
