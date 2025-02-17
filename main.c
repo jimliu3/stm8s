f6597de8cffd3ebab5c520677ef7073d6deb0c2b
Tetsu <kssh310189@gmail.com>
2025-02-17 10:13:48 +0800

    stm8s: add internal flash
    1:Organize files
    2:add internal flash
    
    Signed-off-by: Tetsu <kssh310189@gmail.com>

--- main.c
+++ main.c
@@ -1,43 +1,58 @@
-/* MAIN.C file
- * 
- * Copyright (c) 2002-2005 STMicroelectronics
- */
-
-
 #include "stm8s.h"
 #include "stm8s_conf.h"
 #include "lcd_i2c.h"
 
 int testValue = 0;
 
+#define FLASH_START_ADDR  0x4000  
+#define LED_PORT          GPIOB   
+#define LED_PIN           GPIO_PIN_5  
+uint8_t data1,data2;
+uint32_t i;
+uint8_t Flash_ReadData(uint32_t address);
+
 void GPIO_setup(void);
 void lcd_count(void);
 void SPI_print(void);
+void GPIO_Init_LED(void);
+void LED_Toggle(void);
+void Flash_WriteData(void);
+void Flash_Verify(void);
 
 const char input_string2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 main()
 {
 	//I2C_Clock_Config();
-	SPI_Clock_Config();
-	GPIO_setup();
-	SPI_setup();
-	MX_TIM4_Init();
+	//SPI_Clock_Config();
+	//GPIO_setup();
+	//SPI_setup();
+	//MX_TIM4_Init();
 	//lcd_setup();
-	MAX7219_init();
-	rim();
+	//MAX7219_init();
+	//rim();
 	
 	//
-	  display_clear(); //Clearing the display
-		delay_ms(1000);
-
+	  //display_clear(); //Clearing the display
+		//delay_ms(1000);
+	
+	GPIO_Init_LED(); 
+    Flash_WriteData(); 
+    Flash_Verify(); 
+    
 	while (1) {
 		//Delay_ms_int(100);
 		//GPIO_WriteReverse(GPIOA, GPIO_PIN_3);
 		
 		//lcd_count();
 		
-		SPI_print();
+		//SPI_print();
+		if (data1 == 0x01 && data2 == 0x02) {
+        	LED_Toggle();
+			for (i = 0; i < 5000; i++);
+    	} else {
+			GPIO_WriteHigh(LED_PORT, LED_PIN);		
+    	}
 	}
 
 }
@@ -64,4 +79,39 @@ void SPI_print(void)
 		display_clear(); //Clearing the Display
 		display_string(input_string2);  //Displaying a String
 		delay_ms(2000);
+}
+
+void GPIO_Init_LED(void) {
+    GPIO_DeInit(LED_PORT);
+    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_FAST);
+}
+
+
+void LED_Toggle(void) {
+    GPIO_WriteReverse(LED_PORT, LED_PIN);
+}
+
+
+void Flash_WriteData(void) {
+    FLASH_Unlock(FLASH_MEMTYPE_DATA); 
+
+    FLASH_ProgramByte(FLASH_START_ADDR, 0x01); 
+    FLASH_ProgramByte(FLASH_START_ADDR + 1, 0x02); 
+	FLASH_ProgramByte(FLASH_START_ADDR + 2, 0x03);
+    FLASH_ProgramByte(FLASH_START_ADDR + 3, 0x04);
+	FLASH_ProgramByte(FLASH_START_ADDR + 4, 0x05);
+	FLASH_ProgramByte(FLASH_START_ADDR + 5, 0x0F);
+    FLASH_Lock(FLASH_MEMTYPE_DATA); 
+}
+
+
+uint8_t Flash_ReadData(uint32_t address) {
+    return FLASH_ReadByte(address);
+}
+
+
+
+void Flash_Verify(void) {	
+    data1 = Flash_ReadData(FLASH_START_ADDR);
+    data2 = Flash_ReadData(FLASH_START_ADDR + 1);
 }
